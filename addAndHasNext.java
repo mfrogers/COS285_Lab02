da2b3ff432a7e0397802069b4cfb6eec5eb6fcb9
James543R <144176620+James543R@users.noreply.github.com>
2023-09-25 19:46:49 -0400

    Update addAndHasNext.java

--- addAndHasNext.java
+++ addAndHasNext.java
@@ -1,10 +1,14 @@
 import java.util.NoSuchElementException;
 
+//Author - James Libby
 public class addAndHasNext {
-    
+
+    //adds a fraction to an array list of fractions
     public void add(Fraction frc) {
         return elements.add(element);
     }   
+
+    //returns true if an arrayList has a next value and false if it doesn't have a next value
     public boolean hasNext() {
         try {
             Fraction next = this.next();
